4350f9b48adff3b13249112708de62b5ceeacfbb
alex5272372 <alex5272372@gmail.com>
2020-07-20 09:16:59 +0300

    Postgresql connect

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
+const { Client } = require('pg')
+const client = new Client({
+  user: 'postgres',
+  host: 'procmain.eu',
+  database: 'acc',
+  password: 'FS9final',
+  port: 5432
+})
+
 module.exports = function(app) {
   app.get('/', async function(req, res) {
-    res.render('main');
+    await client.connect()
+    const result = await client.query('SELECT $1::varchar AS my_first_query', ['node hero'])
+    console.log(result)
+    res.render('main')
+    await client.end()
   });
 }
