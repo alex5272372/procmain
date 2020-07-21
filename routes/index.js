c86a23f74411c573f7f4b38dd7c4c8716b308d6d
alex5272372 <alex5272372@gmail.com>
2020-07-21 09:21:04 +0300

    Table of users

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,7 @@ module.exports = function(app) {
   app.get('/', async function(req, res) {
     await client.connect()
     const result = await client.query('SELECT $1::varchar AS my_first_query', ['node hero'])
-    console.log(result)
-    res.render('main')
+    res.render('main', { content: 'dashboard', data: result.rows[0].my_first_query })
     await client.end()
   });
 }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,17 @@
+const { Client } = require('pg')
+const client = new Client({
+  user: 'postgres',
+  host: 'procmain.eu',
+  database: 'acc',
+  password: 'FS9final',
+  port: 5432
+})
+
 module.exports = function(app) {
   app.get('/users', async function(req, res) {
-    res.render('main');
+    await client.connect()
+    const result = await client.query('SELECT id, name FROM users')
+    res.render('main', { content: 'users', rows: result.rows })
+    await client.end()
   });
 }
