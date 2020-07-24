07a47b7a951b2f18efc6200fcaaf0b473cfb01db
alex5272372 <alex5272372@gmail.com>
2020-07-24 18:52:29 +0300

    Display groups

--- routes/groups.js
+++ routes/groups.js
@@ -4,7 +4,11 @@ const pool = new Pool()
 module.exports = function(app) {
   app.get('/groups', async function(req, res) {   
     try {
-      const result = await pool.query('SELECT id, user_id, role_id FROM groups')
+      const result = await pool.query(`
+        SELECT groups.id AS id, users.name AS user_name, roles.name AS role_name FROM groups
+        JOIN users ON groups.user_id = users.id
+        JOIN roles ON groups.role_id = roles.id
+      `)
       res.render('main', { list: 'groups', detail: false, rows: result.rows })
     } catch (err) {
       console.log(err.message)
