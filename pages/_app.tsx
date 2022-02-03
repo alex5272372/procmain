cff0239d3047bf9a5112ccc6c51ca8ef387d6f5c
Oleksii Nikolaienko <alex5272372@gmail.com>
2022-02-03 19:29:54 +0200

    Next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+import { SessionProvider } from "next-auth/react"
+import { AppProps } from "next/app";
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps: { session, ...pageProps }}: AppProps) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
 
-export default MyApp
+export default App
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from '../../../lib/prisma'
+
+const options = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    })
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET
+};
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
